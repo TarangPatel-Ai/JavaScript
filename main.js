becb7b93167575aa7c17675ead2f55aa8eda87f4
TarangPatel <tarangpatel2025@gmail.com>
2025-05-06 12:10:35 +0530

    Update the main script file

--- main.js
+++ main.js
@@ -3,7 +3,23 @@ const Hello = "Hello World!";
 
 let Hello1 = "How Are you..?";
 
-alert(Hello);
+//alert(Hello);
 
 document.getElementById('title').innerText = "Hello JavaScript How Are You...?"
 
+//Number Data
+let n1 = 10;
+let n2 = 20;
+
+//Some opretors
+let n3 = n1 + n2;
+alert('n3 is ' + n3);
+
+//Boolean Data
+let b1 = true;
+let b2 = false;
+
+
+//string(text) can be put in double qoute or in single quote
+let s1 = "Hello World!";
+let s2 = 'Hello JavaScript';
